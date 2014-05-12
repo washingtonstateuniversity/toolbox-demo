c124d20d3d06139baa988cb3ff070f146830f67e
jeremyfelt <jeremy.felt@gmail.com>
2014-05-12 12:06:00 -0700

    Document functionality and cleanup some variable usage

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,10 @@
 (function ($, window) {
+
 	$(document).ready(function(){
+		// Add Fluidbox to any images designated for modals.
 		$("a.modal-image").fluidbox({ stackIndex: 99999 });
 
+		// Load new page content into the next-content-container via PJAX
 		$('.page-anchor').on('click', function(e) {
 			e.preventDefault();
 			$.pjax.click(e, { container: '#next-content-container', scrollTo: false } );
@@ -9,19 +12,52 @@
 	});
 
 	$(document).on('pjax:success', function(e){
-		var scroll_to = Math.floor( $('#main-replacement img' ).offset().top );
-		$('#primary-content' ).attr('data-' + (scroll_to - 200), 'transform: scaleX(1) scaleY(1);' );
-		$('#primary-content' ).attr('data-' + (scroll_to - 25), 'transform: scaleX(.25) scaleY(.75);');
-		var s = skrollr.init();
+		/**
+		 * The container for the primary piece of content on the page.
+		 *
+		 * @type {*|HTMLElement}
+		 */
+		var primary_content = $('#primary-content');
 
-		var new_content = $('#next-content-container').html();
-		$('#main-replacement').append( new_content );
-		$('#next-content-container').html(' ');
-		$('html, body' ).animate({
-			scrollTop: scroll_to }, 1500, 'easeOutCubic', function(){
+		/**
+		 * The container which will initially hold the content to be loaded
+		 * on the page after "navigation".
+		 *
+		 * @type {*|HTMLElement}
+		 */
+		var next_content_container = $('#next-content-container');
+
+		/**
+		 * The top of the replacement content's image. We use this to determine
+		 * where the page should be scrolled to initially.
+		 *
+		 * @type {number}
+		 */
+		var scroll_to = Math.floor( $('#main-replacement img' ).offset().top );
+
+		// Add some Skrollr data attributes to initiate CSS transformations on scroll.
+		primary_content.attr('data-' + (scroll_to - 200), 'transform: scaleX(1) scaleY(1);' );
+		primary_content.attr('data-' + (scroll_to - 25), 'transform: scaleX(.25) scaleY(.75);');
+
+		// Initialize Skrollr so that all data attributes on the page are processed.
+		skrollr.init();
+
+		// Add the replacement content to our replacement container and remove it from
+		// the temporary placeholder node.
+		$('#main-replacement').append( next_content_container.html() );
+		next_content_container.html(' ');
+
+		/**
+		 * Initiate an animation that scrolls the page up to the top of the next content's
+		 * main image. Skrollr handles the transform properties via CSS. Once the scroll
+		 * is completed, we remove the primary content and set the scroll to the top.
+		 */
+		$('html, body' ).animate({ scrollTop: scroll_to }, 1500, 'easeOutCubic', function(){
 			$('#primary-content' ).remove();
 			$(document).scrollTop(0);
 		});
+
+		// Override the back button to go back to the URL stored in history.
 		$(window).on("popstate", function() {
 			window.location.href = $(location ).attr('href');
 			return false;
