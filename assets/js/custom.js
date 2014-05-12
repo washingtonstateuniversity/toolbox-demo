83f468504ef6efd550efbfb0849a768428cf5387
jeremyfelt <jeremy.felt@gmail.com>
2014-05-12 11:53:52 -0700

    Introduce Skrollr.js to handle Parallax behavior
    This now allows us to achieve the Medium effect for scrolling new
    page content up to replace the old.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,4 @@
-(function ($) {
+(function ($, window) {
 	$(document).ready(function(){
 		$("a.modal-image").fluidbox({ stackIndex: 99999 });
 
@@ -9,8 +9,22 @@
 	});
 
 	$(document).on('pjax:success', function(e){
+		var scroll_to = Math.floor( $('#main-replacement img' ).offset().top );
+		$('#primary-content' ).attr('data-' + (scroll_to - 200), 'transform: scaleX(1) scaleY(1);' );
+		$('#primary-content' ).attr('data-' + (scroll_to - 25), 'transform: scaleX(.25) scaleY(.75);');
+		var s = skrollr.init();
+
 		var new_content = $('#next-content-container').html();
 		$('#main-replacement').append( new_content );
 		$('#next-content-container').html(' ');
+		$('html, body' ).animate({
+			scrollTop: scroll_to }, 1500, 'easeOutCubic', function(){
+			$('#primary-content' ).remove();
+			$(document).scrollTop(0);
+		});
+		$(window).on("popstate", function() {
+			window.location.href = $(location ).attr('href');
+			return false;
+		});
 	});
-}(jQuery));
+}(jQuery, window));
