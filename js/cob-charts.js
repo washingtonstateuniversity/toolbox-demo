41b76419cd264ae2c256639c6ddd3e47647cc12f
jeremyfelt <jeremy.felt@gmail.com>
2014-05-14 08:54:24 -0700

    Adjust the margins of the chart for better display

--- js/cob-charts.js
+++ js/cob-charts.js
@@ -30,7 +30,7 @@
 			chart = nv.models.multiBarHorizontalChart()
 				.x(function(d) { return d.label })
 				.y(function(d) { return d.value })
-				.margin({top: 30, right: 20, bottom: 50, left: 120})
+				.margin({top: 40, right: 30, bottom: 40, left: 120})
 //.showValues(true)
 				.tooltips(true)
 //.barColor(d3.scale.category20().range())
